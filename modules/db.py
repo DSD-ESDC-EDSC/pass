ee561af4d5d169d1620a4480e918d787331363f9
Noznoc <julia.conzon@hrsdc-rhdcc.gc.ca>
2020-01-22 16:20:34 -0500

    added db initialization script

--- modules/db.py
+++ modules/db.py
@@ -4,153 +4,33 @@ import csv
 import json
 import osgeo.ogr
 import geopandas as gp
-# from weighted_centroid import *
 from dotenv import load_dotenv
 import os
 import re
 load_dotenv()
 
-DB_HOST = os.environ.get('DB_HOST')
-DB_NAME = os.environ.get('DB_NAME')
-DB_USER = os.environ.get('DB_USER')
-DB_PASSWORD = os.environ.get('DB_PASSWORD')
-
-pg_pool = pool.SimpleConnectionPool(1,5, user=DB_USER, password=DB_PASSWORD, host=DB_HOST, database=DB_NAME)
-
-class dbConnect:
+class DbConnect:
 	def __init__(self):
-		self.conn = pg_pool.getconn()
-		self.cur = self.conn.cursor()
+		self.host = os.environ.get('DB_HOST')
+		self.name = os.environ.get('DB_NAME')
+		self.user = os.environ.get('DB_USER')
+		self.password = os.environ.get('DB_PASSWORD')
 
 	def __enter__(self):
+		self.pg_pool = pool.SimpleConnectionPool(1,5, user=self.user, password=self.password, host=self.host, database=self.name)
+		self.conn = self.pg_pool.getconn()
+		self.cur = self.conn.cursor()
 		return self
 
 	def __exit__(self, type, value, traceback):
 		self.cur.close()
-		pg_pool.putconn(self.conn)
+		self.pg_pool.putconn(self.conn)
 
 # function that creates the geography table
-def initSchema(data_file=None, geo_file=None):
-	with dbConnect() as dbConn:
 
-		initDemand(dbConn)
-
-		dbConn.cur.execute("""
-		DROP TABLE IF EXISTS geo_canada;
-		CREATE TABLE geo_canada(
-		id serial PRIMARY KEY,
-		name text,
-		code int,
-		geom geometry(geometry,4326))
-		""")
-		dbConn.conn.commit()
-
-		if geo_file:
-			data = json.load(geo_file)
-		else:
-			with open('../data/nilfa.geojson', 'r') as f:
-				data = json.load(f)
-
-		query = """
-			INSERT INTO geo_canada(name, code, geom)
-			VALUES(%s, %s, ST_SetSRID(ST_GeomFromGeoJSON(%s), 4326))"""
-
-		for feature in data['features']:
-			#print(feature['properties']['name'])
-			geometry = json.dumps(feature['geometry'])
-			name = feature['properties']['name']
-			code = feature['properties']['code']
-			dbConn.cur.execute(query, (name, code, geometry))
-
-		dbConn.conn.commit()
-
-def initPOI(dbConn, data_file=None):
-	# create demand table
-	dbConn.cur.execute("""
-		DROP TABLE IF EXISTS poi;
-		CREATE TABLE poi(
-		id serial PRIMARY KEY,
-		geoUID text,
-		name text,
-		type text,
-		address text,
-		city text,
-		lng float,
-		ltd float,
-		supply int)
-	""")
-
-	if data_file:
-			next(data_file)
-			dbConn.cur.copy_from(data_file, 'poi', sep='|', columns=('name','code','region','longitude','latitude','total'))
-	else:
-		with open('../data/poi.csv', 'r') as data:
-			#reader = csv.reader(data)
-			#list = [row for row in reader]
-			next(data)
-			dbConn.cur.copy_from(data, 'poi', sep='|', columns=('geoUID','name','type','address','city','lng','ltd','supply'))
-
-		dbConn.conn.commit()
-
-		dbConn.cur.execute("""
-		ALTER TABLE poi ADD COLUMN point geometry(POINT,4326);
-		UPDATE poi SET point = ST_SetSRID(ST_MakePoint(lng,ltd),4326);
-		CREATE INDEX idx_geography_geom ON poi USING GIST(point);
-		""")
-		dbConn.conn.commit()
-
-def initDemand(dbConn, weight, population):
-
-	# read shapefiles for large (lg) (e.g., Dissemination Areas) and small (sm) (e.g., Dissemination Blocks) geometric boundaries
-	boundary_lg_gdf = read_files("../data/demand_lg_pop_mtl.shp", 'shape')
-	boundary_sm_gdf = read_files("../data/demand_sm_pop_mtl.shp", 'shape')
-	print(boundary_lg_gdf.head())
-	# make sure the return is an updated boundary_lg_gdf
-	boundary_lg_gdf = calculate_centroid(boundary_sm_gdf, boundary_lg_gdf, "DAUID", weight)
-
-	boundary_lg_ogr = osgeo.ogr.Open(boundary_lg_gdf.to_json())
-
-	print(list(boundary_lg_gdf))
-	print(boundary_lg_gdf.head())
-
-	# create demand table
-	dbConn.cur.execute("""
-		DROP TABLE IF EXISTS demand;
-		CREATE TABLE demand(
-		id serial PRIMARY KEY,
-		geoUID int,
-		centroid geometry,
-		boundary geometry,
-		pop int)
-	""")
-	dbConn.conn.commit()
-
-	def addBoundary(layer, uid):
-
-		# loop through all features
-		for i in range(layer.GetFeatureCount()):
-			feature = layer.GetFeature(i)
-			fuid = feature.GetField(uid)
-			centroid = feature.GetField('centroid')
-			if centroid.startswith('POINT (-n') or centroid.startswith('POINT (n'):
-				centroid = feature.GetGeometryRef().Centroid().ExportToWkt()
-			pop = feature.GetField(population)
-			geometry = feature.GetGeometryRef()
-			wkt = geometry.ExportToWkt()
-			dbConn.cur.execute("INSERT INTO demand (geoUID, boundary, centroid, pop) VALUES (%s,ST_SetSRID(ST_GeomFromText(%s),3347),%s,%s);", (fuid,wkt,centroid,pop))
-
-		dbConn.conn.commit()
-
-	addBoundary(boundary_lg_ogr.GetLayer(0), "DAUID")
-
-	# create index for demand table
-	# dbConn.cur.execute("CREATE INDEX demand_index ON demand USING GIST(centroids, boundary);")
-	# dbConn.conn.commit()
-
-	# Push data into db
-
+## IMPROVE THIS
 def getPOI():
-	with dbConnect() as dbConn:
+	with DbConnect() as dbConn:
 
 		dbConn.cur.execute("""
 		SELECT json_build_object('name', name, 'type', type, 'geometry', ARRAY[ST_X(ST_Transform(point, 4326)),ST_Y(ST_Transform(point, 4326))], 'supply', supply)
@@ -161,7 +41,7 @@ def getPOI():
 		return records
 
 def getDemand(type):
-	with dbConnect() as dbConn:
+	with DbConnect() as dbConn:
 
 		if type == 'boundary':
 			dbConn.cur.execute("""
---
+++ modules/db_init.py
@@ -0,0 +1,169 @@
+"""
+Created on Tue Jan 22 2020
+
+@author: Noznoc
+
+DESCRIPTION:
+This module manages initializes the PostgreSQL database
+"""
+
+import psycopg2 as pg
+from psycopg2 import pool
+import csv
+import json
+import osgeo.ogr
+import geopandas as gp
+from weighted_centroid import *
+
+class InitSchema(object):
+    """Initialize the PostgreSQL database
+
+    Attributes:
+        self.db_conn (object):
+            database connection
+    """
+
+    def __init__(self, demand_path_lg, poi_path, demand_path_sm=None):
+        """Create the PostgreSQL database tables
+
+        Arguments:
+            demand_path_lg (str):
+                path for demand geodata geodata file
+            poi_path (str):
+                path for POI csv file
+            demand_path_sm (str):
+                path for smaller demand geodata file
+        """
+
+        from db import DbConnect
+        with DbConnect() as db_conn:
+            self.db_conn = db_conn
+            self.create_schema(demand_path_lg, poi_path, demand_path_sm)
+
+    # TO DO function for dynamic reading data files and importing them into db based on values
+
+    def execute_query(self, query):
+        """Execute query to either create or store data into database table
+
+        Arguments:
+            query (str):
+                query to execute
+        """
+
+        self.db_conn.cur.execute(query)
+        self.db_conn.conn.commit()
+
+    def create_schema(self, demand_path_lg, poi_path, demand_path_sm):
+        """Create each PostgreSQL database table
+
+        Arguments:
+            demand_path_lg (str):
+                path for demand geodata geodata file
+            poi_path (str):
+                path for POI csv file
+            demand_path_sm (str):
+                path for smaller demand geodata file
+        """
+        self.init_demand("demand_sm_", "demand_lg_",  "DAUID", demand_path_lg, demand_path_sm)
+        self.init_poi(poi_path)
+		# self.initDistanceMatrix()
+
+    def init_poi(self, poi_path):
+        """Create the poi PostgreSQL database table
+
+        Arguments:
+            poi_path (str):
+                path for POI csv file
+        """
+
+        # create demand table
+        query_create = """
+        	DROP TABLE IF EXISTS poi;
+        	CREATE TABLE poi(
+        	id serial PRIMARY KEY,
+        	geoUID text,
+        	name text,
+        	type text,
+        	address text,
+        	city text,
+        	lng float,
+        	ltd float,
+        	supply int)
+        """
+
+        self.execute_query(query_create)
+
+        with open(poi_path, 'r') as data:
+        	next(data)
+        	self.db_conn.cur.copy_from(data, "poi", sep="|", columns=("geoUID","name","type","address","city","lng","ltd","supply"))
+        	self.db_conn.conn.commit()
+
+        query_alter = """
+        	ALTER TABLE poi ADD COLUMN point geometry(POINT,4326);
+        	UPDATE poi SET point = ST_SetSRID(ST_MakePoint(lng,ltd),4326);
+        	CREATE INDEX idx_poi ON poi USING GIST(point);
+        """
+        self.execute_query(query_alter)
+
+    def init_demand(self, weight, population, uid, demand_path_lg, demand_path_sm=None):
+        """Create the demand PostgreSQL database table
+
+        Arguments:
+            weight (str):
+                population weight column name in the small demand geodata file
+            population (str):
+                population column name in the large demand geodata file
+            uid (str):
+                unique id column name in the large demand geodata file
+            demand_path_lg (str):
+                path for large demand geodata file
+            demand_path_sm (str):
+                path for smaller demand geodata file
+        """
+
+        boundary_lg_gdf = gp.read_file(demand_path_lg)
+        boundary_sm_gdf = gp.read_file(demand_path_sm)
+        boundary_lg_gdf = calculate_centroid(boundary_sm_gdf, boundary_lg_gdf, uid, weight)
+        boundary_lg_gdf = osgeo.ogr.Open(boundary_lg_gdf.to_json())
+        layer = boundary_lg_gdf.GetLayer(0)
+
+        # create demand table
+        query_create = """
+        	DROP TABLE IF EXISTS demand;
+        	CREATE TABLE demand(
+        	id serial PRIMARY KEY,
+        	geoUID int,
+        	centroid geometry,
+        	boundary geometry,
+        	pop int)
+        """
+        self.execute_query(query_create)
+
+        # loop through all features
+        for i in range(layer.GetFeatureCount()):
+            feature = layer.GetFeature(i)
+            fuid = feature.GetField(uid)
+            centroid = feature.GetField("centroid")
+            if centroid.startswith("POINT (-n") or centroid.startswith("POINT (n"):
+            	centroid = feature.GetGeometryRef().Centroid().ExportToWkt()
+            pop = feature.GetField(population)
+            geometry = feature.GetGeometryRef()
+            wkt = geometry.ExportToWkt()
+            self.db_conn.cur.execute("INSERT INTO demand (geoUID, boundary, centroid, pop) VALUES (%s,ST_SetSRID(ST_GeomFromText(%s),3347),%s,%s);", (fuid,wkt,centroid,pop))
+            self.db_conn.conn.commit()
+
+        # create index for demand table
+        self.db_conn.cur.execute("CREATE INDEX idx_demand ON demand USING GIST(centroid, boundary);")
+        self.db_conn.conn.commit()
+
+def main():
+    """Runs script as __main__. """
+
+    demand_path_lg = "../data/demand_lg_pop_mtl.shp"
+    demand_path_sm = "../data/demand_sm_pop_mtl.shp"
+    poi_path = "../data/poi.csv"
+
+    InitSchema(demand_path_lg, poi_path, demand_path_sm)
+
+if __name__ == "__main__":
+    main()
