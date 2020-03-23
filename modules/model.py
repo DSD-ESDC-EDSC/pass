67b66a0bbac8bfc0fe64b29b43333fa788a1ff6f
Noznoc <juliaconzon@gmail.com>
2020-03-23 13:37:35 -0400

    dist threshold

--- modules/model.py
+++ modules/model.py
@@ -53,14 +53,14 @@ def accessibility(bounds, beta, transportation, threshold):
         poi_array = np.array(poi['geouid'])
         supply_array = np.array(poi['supply'])
 
-    # TO DO: INSTEAD OF THIS UPDATE db_init's init_distance_matrix to store as float, not numeric
-    # floats = [col + "::float" for col in poi_array]
+    # script to derive column and population demand geouids that need to be in data frame
     cols = ", poiuid_".join(poi_array)
     ids = ", ".join(map(str, geouid_array))
     dist = ["poiuid_" + col + " <= " + str(threshold) for col in poi_array]
-    where = " OR ".join(dist)
+    where = " OR ".join(dist) # where statement to get all distances equal to or within threshold
 
     # create data frame of distance matrix by first subsetting it based on the geouids and poiuids
+    # filter also by distance threshold
     dm_query = """
         SELECT geouid, poiuid_%s
         FROM distance_matrix_%s
@@ -73,9 +73,11 @@ def accessibility(bounds, beta, transportation, threshold):
         distance_matrix = pd.DataFrame(db_conn.cur.fetchall(), columns=[desc[0] for desc in db_conn.cur.description])
         geouid_filtered_array = np.array(distance_matrix['geouid'])
         distance_matrix = distance_matrix.drop('geouid', axis=1)
-        
+    
+    # store the population demand geouids that are within the threshold
     ids_filtered = ", ".join(map(str, geouid_filtered_array))
     
+    # get the population demand data now with the filtered geouids
     demand_filtered_query = """
         SELECT geouid, ST_AsText(ST_Transform(boundary, 4326)) as boundary, pop
         FROM demand
@@ -88,20 +90,20 @@ def accessibility(bounds, beta, transportation, threshold):
         demand_filtered = pd.DataFrame(db_conn.cur.fetchall(), columns=[desc[0] for desc in db_conn.cur.description])
         demand_filtered_array = np.array(demand_filtered['pop'])
     
-    geouid_array_len = str(len(geouid_array))
-    demand_array_len = str(len(demand_filtered_array))
-    poi_array_len = str(len(poi_array))
-    supply_array_len = str(len(supply_array))
-    distance_matrix_col_len = str(list(distance_matrix.columns.values))
-    distance_matrix_row_len = str(len(distance_matrix.index))
+    # geouid_array_len = str(len(geouid_array))
+    # demand_array_len = str(len(demand_filtered_array))
+    # poi_array_len = str(len(poi_array))
+    # supply_array_len = str(len(supply_array))
+    # distance_matrix_col_len = str(list(distance_matrix.columns.values))
+    # distance_matrix_row_len = str(len(distance_matrix.index))
 
     # for now just to confirm subsetting is correct and lengths match what is in the distance matrix
-    logger.info(f'ARRAY LENGTH OF DEMAND CENTROID COUNTS: {geouid_array_len}')
-    logger.info(f'ARRAY LENGTH OF DEMAND POPULATION COUNTS BASED ON FILTERED DISTANCE THRESHOLD: {demand_array_len}')
-    logger.info(f'ARRAY LENGTH OF SUPPLY SITE COUNTS: {poi_array_len}')
-    logger.info(f'ARRAY LENGTH OF SUPPLY COUNTS: {supply_array_len}')
-    logger.info(f'COLUMNS OF DISTANCE MATRIX: {distance_matrix_col_len}')
-    logger.info(f'ROWS OF DISTANCE MATRIX: {distance_matrix_row_len}')
+    # logger.info(f'ARRAY LENGTH OF DEMAND CENTROID COUNTS: {geouid_array_len}')
+    # logger.info(f'ARRAY LENGTH OF DEMAND POPULATION COUNTS BASED ON FILTERED DISTANCE THRESHOLD: {demand_array_len}')
+    # logger.info(f'ARRAY LENGTH OF SUPPLY SITE COUNTS: {poi_array_len}')
+    # logger.info(f'ARRAY LENGTH OF SUPPLY COUNTS: {supply_array_len}')
+    # logger.info(f'COLUMNS OF DISTANCE MATRIX: {distance_matrix_col_len}')
+    # logger.info(f'ROWS OF DISTANCE MATRIX: {distance_matrix_row_len}')
 
     try:
         model = aceso.ThreeStepFCA(decay_function='negative_power', decay_params={'beta': beta})
@@ -114,7 +116,8 @@ def accessibility(bounds, beta, transportation, threshold):
     except Exception as e:
         logger.error(f'Unsuccessfully calculated accessibility scores: {e}')
         return e
-
+    
+    # TO DO: MAKE SCORE MORE INTERPRETABLE
     demand_filtered['scores'] = demand_filtered['scores'].apply(lambda x: x * 10000)
     
     return demand_filtered
