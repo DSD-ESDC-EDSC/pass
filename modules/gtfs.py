d3f5878188b8030b9f5d0f73bac45040e40e7377
Noznoc <juliaconzon@gmail.com>
2020-04-24 13:38:17 -0400

    added loop to retrieve all data from each page

--- modules/gtfs.py
+++ modules/gtfs.py
@@ -11,24 +11,35 @@ load_dotenv()
 key = os.environ.get('GTFS_API_KEY') # obtain a key through GitHub signup to OpenMobilityData
 location = '32' # location 32 is Canada
 
-# retrieves feeds based on location specified (currently all GTFS feeds in Canada)
-url_feeds = 'https://api.transitfeeds.com/v1/getFeeds?key='+key+'&location='+location+'&descendants=1&page=1&limit=300'
+# first, get the number of pages to retrieve all GTFS feeds in Canada
+url_pages = 'https://api.transitfeeds.com/v1/getFeeds?key='+key+'&location='+location+'&descendants=1&limit=100'
+page_number = json.loads(requests.get(url_pages).content)['results']['numPages']
 
-# store GTFS feeds within Canada
-feeds = json.loads(requests.get(url_feeds).content)
+# loop through the pages and get all feeds within each page
+for page in range(page_number):
+    # page starts at 1, not 0
+    page += 1
+    
+    # retrieves feeds based on location specified (currently all GTFS feeds in Canada)
+    url_feeds = 'https://api.transitfeeds.com/v1/getFeeds?key='+key+'&location='+location+'&descendants=1&page='+page+'&limit=100'
 
-# get zipped feed GTFS data per each feed
-for feed in feeds['results']['feeds']:
-    
-    # store feed id
-    feed_id = feed['id']
-    
-    # create url to get specific feed from API
-    url_get = 'https://api.transitfeeds.com/v1/getLatestFeedVersion?key='+key+'&feed='+feed_id
-    
-    # save downloaded feed into a data/gtfs folder
-    with open('../data/gtfs/'+feed_id.replace('/','_')+'.zip', 'wb') as f:
-        f.write(requests.get(url_get).content)
-        f.close()
+    # store GTFS feeds within Canada
+    feeds = json.loads(requests.get(url_feeds).content)
+
+    # get zipped feed GTFS data per each feed
+    for feed in feeds['results']['feeds']:
+        
+        # store feed id
+        feed_id = feed['id']
+        
+        # create url to get specific feed from API
+        url_get = 'https://api.transitfeeds.com/v1/getLatestFeedVersion?key='+key+'&feed='+feed_id
+        
+        # save downloaded feed into a data/gtfs folder
+        with open('../data/gtfs/'+feed_id.replace('/','_')+'.zip', 'wb') as f:
+            f.write(requests.get(url_get).content)
+            f.close()
+
+# TO DO: come up with a method for updating existing data files using the /getLatestFeedVersion
     
     
